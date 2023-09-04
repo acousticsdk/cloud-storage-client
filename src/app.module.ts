3e2d4962c7ef82f473e1ddfcb8481716c74c755b
acousticsdk <Acousticpwnz1>
2023-09-04 16:40:02 +0300

    users to files connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { FilesModule } from './files/files.module';
+import {TypeOrmModule} from "@nestjs/typeorm";
+import {UserEntity} from "./users/entities/user.entity";
+import {FileEntity} from "./files/entities/file.entity";
 
 @Module({
-  imports: [],
+  imports: [ TypeOrmModule.forRoot({
+    type: 'postgres',
+    host: 'arjuna.db.elephantsql.com',
+    port: 5432,
+    username: 'oxmvkjnc',
+    password: 'kIELGLNZoYf3r_yoRxnRbGZF9wbkEh7R',
+    database: 'oxmvkjnc',
+    entities: [UserEntity, FileEntity],
+    synchronize: true,
+  }),UsersModule, FilesModule],
   controllers: [AppController],
   providers: [AppService],
 })
