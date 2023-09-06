9e1507cb8431df511aecfd15a57cc94435f501fc
acousticsdk <Acousticpwnz1>
2023-09-06 11:39:57 +0300

    Backend

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,15 +6,18 @@ import { FilesModule } from './files/files.module';
 import {TypeOrmModule} from "@nestjs/typeorm";
 import {UserEntity} from "./users/entities/user.entity";
 import {FileEntity} from "./files/entities/file.entity";
+import {ConfigModule} from "@nestjs/config";
 
 @Module({
-  imports: [ TypeOrmModule.forRoot({
+  imports: [
+    ConfigModule.forRoot(),
+      TypeOrmModule.forRoot({
     type: 'postgres',
-    host: 'arjuna.db.elephantsql.com',
-    port: 5432,
-    username: 'oxmvkjnc',
-    password: 'kIELGLNZoYf3r_yoRxnRbGZF9wbkEh7R',
-    database: 'oxmvkjnc',
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
     entities: [UserEntity, FileEntity],
     synchronize: true,
   }),UsersModule, FilesModule],
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import {InjectRepository} from "@nestjs/typeorm";
+import {FileEntity} from "../files/entities/file.entity";
+import {Repository} from "typeorm";
+import {UserEntity} from "./entities/user.entity";
 
 @Injectable()
 export class UsersService {
+  constructor(
+      @InjectRepository(UserEntity)
+      private repository: Repository<UserEntity>
+  ) {
+
+  }
+
   create(createUserDto: CreateUserDto) {
     return 'This action adds a new user';
   }
