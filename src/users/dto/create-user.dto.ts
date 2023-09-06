f1a9c6597612cd0db5899910a0952d2d775ca589
acousticsdk <acoustic.sdk@gmail.com>
2023-09-06 17:09:13 +0300

    Backend

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,5 +1,14 @@
+import {ApiProperty} from "@nestjs/swagger";
+
 export class CreateUserDto {
+    @ApiProperty({
+        default: 'admin@gmail.com'
+    })
     email:string;
+    @ApiProperty({ default: 'Max'})
     fullName: string;
+    @ApiProperty({
+        default: '123'
+    })
     password: string;
 }
