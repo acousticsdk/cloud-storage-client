64d7b96367deacbc72bcdd9780d74491c67e4991
acousticsdk <acoustic.sdk@gmail.com>
2023-09-07 13:14:21 +0300

    Finish backend

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,30 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import {DocumentBuilder, SwaggerModule} from "@nestjs/swagger";
-import * as express from 'express'
-import {join} from 'path'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as express from 'express';
+import { join } from 'path';
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {cors:false});
+  const app = await NestFactory.create(AppModule, { cors: false });
 
-  app.enableCors({credentials:true, origin:true})
+  app.enableCors({ credentials: true, origin: true });
 
-app.use('/uploads', express.static(join(__dirname,'..', 'uploads')))
+  app.use('/uploads', express.static(join(__dirname, '..', 'uploads')));
+
   const config = new DocumentBuilder()
-      .setTitle('Cloud storage')
-      .setVersion('1.0')
-      .addBearerAuth()
-      .build();
+    .setTitle('Облачное хранилище')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
   const document = SwaggerModule.createDocument(app, config);
+
   SwaggerModule.setup('swagger', app, document, {
     swaggerOptions: {
-      persistAuthorization: true
-    }
+      persistAuthorization: true,
+    },
   });
-  await app.listen(8000);
 
+  await app.listen(7777);
 }
 bootstrap();
